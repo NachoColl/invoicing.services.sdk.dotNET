e0426f7c6ca6d62063337cdee39337fe8bac3c00
Nacho Coll <ignacio.coll@gmail.com>
2017-01-11 21:47:55 +0100

    Minor updates.

--- code/invoicing.services.sdk.dotNET.Tests/FunctionTest.cs
+++ code/invoicing.services.sdk.dotNET.Tests/FunctionTest.cs
@@ -9,13 +9,18 @@ namespace invoicing.services.sdk.dotNET.Tests
 {
     public class FunctionTest
     {
+
+        // Set your API KEY here for doing the tests.
+        const string MY_API_KEY = "";
+
         [Fact(DisplayName = "Creates a simple invoice.")]
         public void Test1() {
 
-            InvoicingAPI API = new InvoicingAPI("QpMIwFClmaa1BH5E3UhsJWFLwHW6jhr5ijkgEHqe");
+            InvoicingAPI API = new InvoicingAPI(MY_API_KEY);
 
             long now = Utils.Timestamp.CurrentTimeMillis();
             AddInvoiceResponse response = API.AddInvoice(new Model.Invoice() {
+                Dummy = true,
                 Date = now,
                 Seller = new Model.Actor() {
                     Name = "ACME Corporation",
@@ -38,7 +43,7 @@ namespace invoicing.services.sdk.dotNET.Tests
         [Fact(DisplayName = @"Sets your Seller ""Bill From"" data information.")]
         public void Test2() {
 
-            InvoicingAPI API = new InvoicingAPI("QpMIwFClmaa1BH5E3UhsJWFLwHW6jhr5ijkgEHqe");
+            InvoicingAPI API = new InvoicingAPI(MY_API_KEY);
 
             Model.Actor response = API.UpdateSeller(new Model.Actor() {
                     Id = "My Seller ID",
@@ -59,10 +64,11 @@ namespace invoicing.services.sdk.dotNET.Tests
         [Fact(DisplayName = @"Creates an invoice with your default Seller ""Bill From"" info.")]
         public void Test3() {
 
-            InvoicingAPI API = new InvoicingAPI("QpMIwFClmaa1BH5E3UhsJWFLwHW6jhr5ijkgEHqe");
+            InvoicingAPI API = new InvoicingAPI(MY_API_KEY);
 
             long now = Utils.Timestamp.CurrentTimeMillis();
             AddInvoiceResponse response = API.AddInvoice(new Model.Invoice() {
+                Dummy = true,
                 Date = now,
                 Buyer = new Model.Actor() {
                     Name = "My First Invoice Buyer"
@@ -75,6 +81,86 @@ namespace invoicing.services.sdk.dotNET.Tests
                          ItemTotalAmount = 40.02m
                      } },
                 Totals = new Model.InvoiceTotals() { Total = 40.02m }
+            });
+            Assert.Equal(now.ToString(), response.InvoiceDate);
+        }
+
+        [Fact(DisplayName = @"Creates a more extensive Invoice.")]
+        public void Test4() {
+
+            InvoicingAPI API = new InvoicingAPI(MY_API_KEY);
+
+            long now = Utils.Timestamp.CurrentTimeMillis();
+            AddInvoiceResponse response = API.AddInvoice(new Model.Invoice() {
+                Dummy = true,
+                Id="0000123",
+                Date = now,
+                CurrencyCode = "USD",
+                CountryCode = "US",
+                Seller = new Model.Actor() {
+                    Id="My Seller ID",
+                    Name="I am a Seller",
+                    Line1="This is my address.",
+                    TaxIds= new List<Model.ActorTaxId>() {
+                        new Model.ActorTaxId() {
+                            Name="FEV",
+                            Value="774646/OL"
+                        },
+                        new Model.ActorTaxId() {
+                            Name="FR",
+                            Value="US7758847"
+                        }
+                    }
+                    
+                },
+                Buyer = new Model.Actor() {
+                    Id="INTERNAL 00212",
+                    Name = "My First Invoice Buyer",
+                    Line1 = "Company Address Line 1",
+                    Line2 = "Address Line 2",
+                    TaxIds = new List<Model.ActorTaxId>() {
+                        new Model.ActorTaxId() {
+                            Name = "FR",
+                            Value="US9093930"
+                        }
+                    }
+                },
+                Items = new List<Model.InvoiceItem>() {
+                     new Model.InvoiceItem() {
+                         Name = "My Product Name",
+                         Description = "Product description.",
+                         UnitPrice = 20.01m,
+                         Quantity = 2,
+                         Taxes = new List<Model.Tax>() {
+                             new Model.Tax() {
+                                 TaxName = "VAT",
+                                 TaxRate = 10
+                             }
+                         },
+                         ItemSubTotalAmount = 40.02m,
+                         ItemTotalAmount = 44.02m
+                     } },
+                Totals = new Model.InvoiceTotals() {
+                     SubTotal = 40.02m,
+                     TaxTotals = new List<Model.InvoiceTaxTotal>() {
+                         new Model.InvoiceTaxTotal() {
+                             TaxName="VAT",
+                             TaxRate=10,
+                             TaxTotal=4.00m
+                         }
+                     },
+                    Total = 44.02m
+                },
+                Notes = new Model.TextBlock() {
+                    Line1="This is a note to include some text/conditions."
+                },
+                Labels = new Model.Labels() {
+                    Title = "QUOTATION",
+                    Total = "Total Quotation"
+                },
+                Colors = new Model.Colors() {
+                    Color1 = "#766755"
+                }
             });
             Assert.Equal(now.ToString(), response.InvoiceDate);
         }
