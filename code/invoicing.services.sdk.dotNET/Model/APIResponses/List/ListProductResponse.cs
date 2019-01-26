614f65941ad8be0b97d89737a691e52156b54ea0
Nacho Coll <ignacio.coll@gmail.com>
2019-01-26 16:54:56 +0100

    Product INITIALS

---
+++ code/invoicing.services.sdk.dotNET/Model/APIResponses/List/ListProductResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace invoicing.services.sdk.dotNET.Model.APIResponses.List
+{
+    public class ListProductResponse {
+
+        [JsonProperty(PropertyName = "count")]
+        public int Count { get; set; }
+
+        [JsonProperty(PropertyName = "list")]
+        public List<Product> List { get; set; }
+    }
+}
