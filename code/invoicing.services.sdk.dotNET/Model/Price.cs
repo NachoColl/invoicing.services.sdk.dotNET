ce3c0c9f593959cdcf5da97d75e93b67ad9a598c
Nacho Coll <ignacio.coll@gmail.com>
2019-01-26 16:26:48 +0100

    Model.Product

---
+++ code/invoicing.services.sdk.dotNET/Model/Price.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+
+namespace invoicing.services.sdk.dotNET.Model
+{
+    public class Price
+    {
+        [JsonProperty(PropertyName = "countryCode")]
+        public string CountryCode { get; set; } = "US";
+
+        [JsonProperty(PropertyName = "currencyCode")]
+        public string CurrencyCode {get;set;} = "USD";
+
+        [JsonProperty(PropertyName = "amount")]
+         public decimal Amount {get; set;}  = 0;
+    }
+}
---
+++ code/invoicing.services.sdk.dotNET/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace invoicing.services.sdk.dotNET.Model
+{
+    public class Product
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")] 
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "description")] 
+        public string Description { get; set; }
+        [JsonProperty(PropertyName = "prices")] 
+        public List<Price> Prices { get; set; } = new List<Price>();
+
+    }
+}
